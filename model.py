b2da7454cebe227a37f1508539a28c399bcc643d
Andrew J. Walker <ajw.thunder@gmail.com>
2017-03-11 00:40:05 +1100

    Update model.py

--- model.py
+++ model.py
@@ -59,6 +59,6 @@ model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=1)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
 
 model.save('model.h5')
