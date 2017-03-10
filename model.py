137d6b5d72c58e978a5c78ddba2c5b934beb8352
Andrew <ajw.thunder@gmail.com>
2017-03-11 00:34:08 +1100

    Finish model

--- model.py
+++ model.py
@@ -42,26 +42,11 @@ for image, measurement in zip(images, measurements):
 X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
-def compare_images(left_image, right_image):
-    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
-    f.tight_layout()
-    ax1.imshow(left_image)
-    ax1.set_title('Shape '+ str(left_image.shape), fontsize=50)
-    ax2.imshow(right_image)
-    ax2.set_title('Shape '+ str(right_image.shape), fontsize=50)
-    plt.show()
-
 
 # Model Implementation
 model = Sequential()
 model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70, 25), (1, 1))))
-
-# Display cropping before and after
-#cropping_output = K.function([model.layers[1].input], [model.layers[1].output])
-#cropped_image = cropping_output([image[None,...]])[0]
-#compare_images(image, cropped_image.reshape(cropped_image.shape[1:]))
-
 model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu"))
 model.add(Dropout(0.5))
 model.add(Convolution2D(36,5,5, subsample=(2,2), activation="relu"))
@@ -74,6 +59,6 @@ model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=1)
 
 model.save('model.h5')
